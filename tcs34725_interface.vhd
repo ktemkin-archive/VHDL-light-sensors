715939cc260abaf0bf5199b982a18b9d1239b05a
Kyle Temkin <ktemkin@binghamton.edu>
2014-05-11 02:40:44 -0400

    Add work-in-progress simple interface for TCS34725.

--- tcs34725_interface.vhd
+++ tcs34725_interface.vhd
@@ -21,7 +21,7 @@
 -- THE FIRMWARE.
 
 --
--- Simple TSL2561 driver; supports only a single TSL2561 at a time.
+-- Simple TCS34725 interface.
 --
 
 
@@ -29,22 +29,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity tsl2561_interface is
+entity tcs34725_interface is
   generic(
-
-    --This is the address for a floating ADDR SEL pin.
-    --If the ADDR SEL pin is tied high, provide "1001001" instead.
-    --If the ADDR SEL pin is tied low, provide  "0001100" instead.
-    address : std_logic_vector(6 downto 0) := "0111001";
 
     --The clock frequency of the board you're using.
     --For the Basys board, this is usually 50MHz, or 50_000_000.
     clk_frequency : integer := 50_000_000;
 
     --The I2C clock frequency. This can be any number below 400kHz for
-    --the TSL2561.
+    --the TCS34725.
     i2c_frequency : integer := 100_000
-
   );
   port(
 
@@ -57,36 +51,48 @@ entity tsl2561_interface is
     scl : inout std_logic;
 
     --Light sensor reading...
-    light_intensity : out std_logic_vector(15 downto 0)
+    clear_intensity : out std_logic_vector(15 downto 0);
+    red_intensity   : out std_logic_vector(15 downto 0);
+    blue_intesity   : out std_logic_vector(15 downto 0);
+    green_intensity : out std_logic_vector(15 downto 0)
   );
-end tsl2561_interface;
+end tcs34725_interface;
 
-architecture Behavioral of tsl2561_interface is
+architecture Behavioral of tcs34725_interface is
 
+  --The address of the TCS34725. This device has only one possible address,
+  --so we won't genericize it.
+  constant address : std_logic_vector := "0101001";
 
   --Signals for data exchange with the core I2C controller.
   signal data_to_write, last_read_data : std_logic_vector(7 downto 0);
-  signal read_or_write, transaction_active, controller_in_use : std_logic;
+  signal reading, transaction_active, controller_in_use : std_logic;
 
   --Rising edge detect for the "controller in use" signal.
   --A rising edge of this signal indicates that the I2C controller has accepted our data.
   signal controller_was_in_use    : std_logic;
-  signal controller_accepted_data : std_logic;
+  signal controller_accepted_data, new_data_available : std_logic;
 
   --I2C read/write constants.
   constant write : std_logic := '0';
   constant read  : std_logic := '1';
 
-  --TSL2561 commands.
+  --I2C commands for the TSL34725.
   constant select_control_register : std_logic_vector := x"80";
   constant power_on                : std_logic_vector := x"03";
-  constant read_light_intensity    : std_logic_vector := x"AC";
+  constant read_color_values       : std_logic_vector := x"B4";
 
-
-  --Core state machine logic for the 
+  --Core state machine logic.
   type state_type is (STARTUP, SEND_POWER_COMMAND, TURN_POWER_ON,
-                      WAIT_BEFORE_READING, SEND_READ_COMMAND, READ_LOW_BYTE, READ_HIGH_BYTE);
+                      WAIT_BEFORE_READING, SEND_READ_COMMAND, READ_LOW_BYTE, READ_HIGH_BYTE); --READ_BYTES);
   signal state, next_state : state_type := STARTUP;
+
+  --Create a simple read buffer for each of the sequential bytes.
+  type byte_buffer is array(natural range <>) of std_logic_vector(7 downto 0);
+  signal read_buffer : byte_buffer(7 downto 0);
+
+  --Signal which stores the current index in the byte buffer.
+  signal current_byte_number : integer range 0 to 7;
 
 
 begin
@@ -105,7 +111,7 @@ begin
 		reset_n   => not reset,
 		ena       => transaction_active,
 		addr      => address,
-		rw        => read_or_write,
+		rw        => reading,
 		data_wr   => data_to_write,
 		busy      => controller_in_use,
 		data_rd   => last_read_data,
@@ -122,6 +128,7 @@ begin
   --
   controller_was_in_use    <= controller_in_use when rising_edge(clk);
   controller_accepted_data <= controller_in_use and not controller_was_in_use;
+  new_data_available       <= reading and not controller_in_use and controller_was_in_use;
 
   CONTROL_FSM:
   process(clk)
@@ -159,7 +166,7 @@ begin
           
           --Set up the device to write the first byte of the setup command.
           transaction_active <= '1';
-          read_or_write      <= write;
+          reading      <= write;
 
           --Select the device's primary control register.
           data_to_write      <= select_control_register;
@@ -204,7 +211,7 @@ begin
             -- shouldn't affect any designs. If this affects yours, you'll need to
             -- create a duplicate of this state which is only reached after a successful
             -- reading.)
-            light_intensity(15 downto 8) <= last_read_data;
+            clear_intensity(15 downto 8) <= last_read_data;
 
             state <= SEND_READ_COMMAND;
           end if;
@@ -218,11 +225,13 @@ begin
 
           --Set up the device to write to the command register,
           --indicating that we want to read multiple bytes from the ADC register.
-          transaction_active <= '1';
-          read_or_write      <= write;
+          transaction_active  <= '1';
+          reading       <= write;
+          current_byte_number <= 0;
+          
 
           --Select the device's primary control register.
-          data_to_write      <= read_light_intensity;
+          data_to_write      <= read_color_values;
 
           --Once the controller has accepted the command,
           --move to the state where we'll read from the device itself.
@@ -240,7 +249,8 @@ begin
           --Set up the device to write to the command register,
           --indicating that we want to read multiple bytes from the ADC register.
           transaction_active <= '1';
-          read_or_write      <= read;
+          reading      <= read;
+
 
           --Once the controller has initiated the read
           --move to read the subsequent bit.
@@ -249,7 +259,6 @@ begin
           end if;
 
 
-        --
         -- Read the ADC low byte.
         -- This reads the low byte of the ADC.
         --
@@ -258,7 +267,7 @@ begin
           --If the controller has gone idle, the most recently read data
           --will be the data read in the last state; or the ADC low byte.
           if controller_in_use = '0' then
-            light_intensity(7 downto 0) <= last_read_data;
+            clear_intensity(7 downto 0) <= last_read_data;
           end if;
 
           --Once the controller has accepted the data,
@@ -275,7 +284,6 @@ begin
 
       end case; 
     end if;
-
 
   end process;
 
